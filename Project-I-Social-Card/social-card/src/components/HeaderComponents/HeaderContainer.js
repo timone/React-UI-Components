4e24d646b1830a1bcf59f3217e136c031a1de348
Timur Mone <timur@yurtah.com>
2019-05-29 21:27:45 -0400

    finished mvp1

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent.js';
+import HeaderTitle from './HeaderTitle.js';
+import ImageThumbnail from './ImageThumbnail.js';
+const HeaderContainer = ()=>{
+    return (
+        <div className="container">
+        <ImageThumbnail />
+        <HeaderTitle/>
+        <HeaderContent/>
+        </div>
+    );
+}
+export default HeaderContainer;
